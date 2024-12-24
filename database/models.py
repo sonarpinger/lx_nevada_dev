2291471265458f73917103b80724fb811e288560
sonarpinger <brandonramirez@nevada.unr.edu>
2024-12-23 17:05:50 -0800

    update database models

--- database/models.py
+++ database/models.py
@@ -1,24 +1,65 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import (
+    Column, ForeignKey, Integer, String, Boolean, DateTime, Float, Table, Enum
+)
+from sqlalchemy.types import Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+import datetime
+
+STATUS_ENUM = ('undefined', 'started', 'stopped', 'deleted', 'error')
+USER_TYPE_ENUM = ('student', 'instructor', 'admin')
 
 Base = declarative_base()
 
-# user has username and a list of environments
+def utcnow():
+    return datetime.datetime.now(tz=datetime.timezone.utc)
+
+user_courses = Table(
+    'user_courses',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id', ondelete="CASCADE"), primary_key=True),
+    Column('course_id', Integer, ForeignKey('courses.id', ondelete="CASCADE"), primary_key=True)
+)
+
 class User(Base):
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), nullable=False, unique=True)
+    user_type = Column(Enum(*USER_TYPE_ENUM), default='student', nullable=False)
+    totp_secret = Column(String(50), nullable=True)
+    is_totp_enabled = Column(Boolean, default=True)
+    is_totp_confirmed = Column(Boolean, default=False)
 
-    # Define the relationship to Environment
-    # User can have many environments
-    environments = relationship("Environment", back_populates="users")
+    # Relationships
+    environments = relationship(
+        "Environment",
+        back_populates="users",
+        cascade="all, delete-orphan"
+    )
+    courses = relationship(
+        "Course",
+        secondary=user_courses,
+        back_populates="users"
+    )
 
-    # totp 
-    is_totp_enabled = Column(Boolean, default=False)
-    totp_secret = Column(String(50))
+class Course(Base):
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False, unique=True)
 
+    instructors = relationship(
+        "User",
+        secondary=user_courses,
+        back_populates="courses"
+    )
+
+    # Relationships
+    users = relationship(
+        "User",
+        secondary=user_courses,
+        back_populates="courses"
+    )
+    environments = relationship("Environment", back_populates="course")
 
 # environment has machine name, course and ip address
 class Environment(Base):
@@ -26,14 +67,22 @@ class Environment(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     machine_name = Column(String(50), nullable=False)
-    course = Column(String(50), nullable=False)
+    course_id = Column(Integer, ForeignKey('courses.id'))
     vmid = Column(Integer, nullable=False)
     # can be defined later
     ip_address = Column(String(50))
-    status = Column(String(50), default="undefined", nullable=False)
-    
+    status = Column(Enum(*STATUS_ENUM), default='undefined', nullable=False)
+
     # Define the relationship to User
     # Environment can only have one user
     users = relationship("User", back_populates="environments")
     user_id = Column(Integer, ForeignKey('users.id'))
-    
+
+class Service(Base):
+    __tablename__ = 'services'
+
+    id = Column(Integer, primary_key=True, index=True)
+    common_name = Column(String(50), nullable=False)
+    connection_string = Column(String(255), nullable=False)
+    environment_id = Column(Integer, ForeignKey('environments.id'))
+    environment = relationship("Environment", back_populates="services")
